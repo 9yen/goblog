f1cb9411b8b3f3ad8980c538d2fc129b430af570
9yen <3267666759@qq.com>
2023-06-18 15:46:19 +0800

    Change if judgment to switch

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
+	switch r.URL.Path {
+	case "/":
 		_, _ = fmt.Fprint(w, "<h1>Hello! Welcome to Gin's World!</h1>")
-	} else if r.URL.Path == "/about" {
+	case "/about":
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		_, _ = fmt.Fprint(w, "This blog is used to record programming notes, if you have feedback or suggestions, please contact "+"<a href=\"mailto:3267666759@qq.com\">9yen</a>")
-	} else {
+	default:
 		w.WriteHeader(http.StatusNotFound)
 		_, _ = fmt.Fprint(w, "<h1>404 NOT FOUNT :(</h1>")
 	}
