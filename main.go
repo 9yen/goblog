f497e9f926456515ae8926aa5d130c471e41fcfc
9yen <3267666759@qq.com>
2023-06-18 16:54:39 +0800

    尝试http.NewServeMux

--- main.go
+++ main.go
@@ -3,23 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		_, _ = fmt.Fprint(w, "<h1>Hello! Welcome to Gin's World!</h1>")
-	case "/about":
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		_, _ = fmt.Fprint(w, "This blog is used to record programming notes, if you have feedback or suggestions, please contact "+"<a href=\"mailto:3267666759@qq.com\">9yen</a>")
-	default:
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	} else {
 		w.WriteHeader(http.StatusNotFound)
-		_, _ = fmt.Fprint(w, "<h1>404 NOT FOUNT :(</h1>")
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
 func main() {
-	fmt.Println("Hello Gin!")
-	http.HandleFunc("/", handlerFunc)
-	_ = http.ListenAndServe(":3000", nil)
+	r := http.NewServeMux()
+	r.HandleFunc("/", defaultHandler)
+	r.HandleFunc("/about", aboutHandler)
+	// 文章详情
+	r.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		fmt.Fprint(w, "文章 ID："+id)
+	})
+	// 列表 or 创建
+	r.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+	})
+
+	http.ListenAndServe(":3000", r)
 }
