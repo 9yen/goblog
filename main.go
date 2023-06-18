60033e1ae2c9609fe623bf4c85b1cf8462f16189
9yen <3267666759@qq.com>
2023-06-18 23:16:02 +0800

    重构 RouteName2URL

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 	"time"
 	"unicode/utf8"
 
+	"github.com/9yen/goblog/pkg/route"
 	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-var router = mux.NewRouter()
+var router *mux.Router
 var db *sql.DB
 
 func initDB() {
@@ -121,15 +122,15 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		// 4. 读取成功，显示文章
-        tmpl, err := template.New("show.gohtml").
-            Funcs(template.FuncMap{
-                "RouteName2URL": RouteName2URL,
-                "Int64ToString": Int64ToString,
-            }).
-            ParseFiles("resources/views/articles/show.gohtml")
-        checkError(err)
-        err = tmpl.Execute(w, article)
-        checkError(err)
+		tmpl, err := template.New("show.gohtml").
+			Funcs(template.FuncMap{
+				"RouteName2URL": route.Name2URL,
+				"Int64ToString": Int64ToString,
+			}).
+			ParseFiles("resources/views/articles/show.gohtml")
+		checkError(err)
+		err = tmpl.Execute(w, article)
+		checkError(err)
 	}
 }
 
@@ -453,20 +454,9 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// RouteName2URL 通过路由名称来获取 URL
-func RouteName2URL(routeName string, pairs ...string) string {
-    url, err := router.Get(routeName).URL(pairs...)
-    if err != nil {
-        checkError(err)
-        return ""
-    }
-
-    return url.String()
-}
-
 // Int64ToString 将 int64 转换为 string
 func Int64ToString(num int64) string {
-    return strconv.FormatInt(num, 10)
+	return strconv.FormatInt(num, 10)
 }
 
 // Delete 方法用以从数据库中删除单条记录
@@ -488,7 +478,8 @@ func (a Article) Delete() (rowsAffected int64, err error) {
 func main() {
 	initDB()
 	createTables()
-
+	route.Initialize()
+	router = route.Router
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
